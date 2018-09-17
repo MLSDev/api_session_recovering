a9d9918074c4fedf700695a45e0c00515cc31073
Dmytro Stepaniuk <stepaniuk@mlsdev.com>
2018-09-17 14:07:38 +0300

    use mailers generator instead

--- lib/generators/api_session_recovering/mailers_generator.rb
+++ lib/generators/api_session_recovering/mailers_generator.rb
@@ -1,11 +1,11 @@
 module ApiSessionRecovering
   module Generators
-    class ResetPassswordMailerViewsGenerator < Rails::Generators::Base
+    class MailersGenerator < Rails::Generators::Base
       source_root File.expand_path('../../templates/mail_templates/api_session_recovering/', __FILE__)
 
       desc 'Creates a restore_password mail template in your views dir'
 
-      def copy_reset_password_mailer_view
+      def copy_mailer
         copy_file 'restore_password_mailer/email.html.erb', 'app/views/api_session_recovering/restore_password_mailer/email.html.erb'
       end
 
